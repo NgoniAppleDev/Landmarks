711360e5fa8a32b3c0db8159136fe4b1b87d30d4
Ngoni Katsidzira <ngonik8@gmail.com>
2025-01-07 13:16:28 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ngoni Katsidzira  on 7/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
